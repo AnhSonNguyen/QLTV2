3fbd31000914ef8e653865c876c916181b9ea88a
AnhSonNguyen <anhsonnguyen1806@gmail.com>
2025-05-14 19:43:51 +0700

    .

---
+++ QLTV2/Controllers/BooksController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using QLTV2.Models;
+
+namespace QLTV2.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly LibraryDbContext _context;
+
+        public BooksController(LibraryDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var books = await _context.TbBooks
+                .Where(b => b.IsActive)
+                .Include(b => b.Category)
+                .ToListAsync();
+
+            return View(books);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var book = await _context.TbBooks
+                .Include(b => b.Category)
+                .FirstOrDefaultAsync(b => b.BookId == id && b.IsActive);
+
+            if (book == null)
+                return NotFound();
+
+            return View(book);
+        }
+    }
+}

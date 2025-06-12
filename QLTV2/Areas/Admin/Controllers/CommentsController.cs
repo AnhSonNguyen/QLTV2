d0769af6a81c9dabf9d28eb78d04b6a5c6779eb4
AnhSonNguyen <anhsonnguyen1806@gmail.com>
2025-06-12 11:03:01 +0700

    cập nhật thêm quản lý bình luận, sửa phần "Sách đang mượn" bên web

---
+++ QLTV2/Areas/Admin/Controllers/CommentsController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using QLTV2.Models;
+
+namespace QLTV2.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CommentsController : Controller
+    {
+        private readonly LibraryDbContext _context;
+
+        public CommentsController(LibraryDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Comments
+        public async Task<IActionResult> Index()
+        {
+            var comments = await _context.TbBookReviews
+                .Include(c => c.Account)
+                .Include(c => c.Book)
+                .OrderByDescending(c => c.CreatedDate)
+                .ToListAsync();
+
+            return View(comments);
+        }
+
+        // GET: Admin/Comments/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var comment = await _context.TbBookReviews
+                .Include(c => c.Account)
+                .Include(c => c.Book)
+                .FirstOrDefaultAsync(m => m.BookReviewId == id);
+
+            if (comment == null)
+                return NotFound();
+
+            return View(comment);
+        }
+
+        // POST: Admin/Comments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var comment = await _context.TbBookReviews.FindAsync(id);
+            if (comment != null)
+            {
+                _context.TbBookReviews.Remove(comment);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
